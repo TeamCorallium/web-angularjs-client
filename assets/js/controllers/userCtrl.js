67f3d3386fbb1f9b9d1d40fe3a76f5b249e1bed9
alejandro ravelo julian <aravelo@uci.cu>
2017-05-03 15:52:16 -0400

    adding function log out

--- assets/js/controllers/userCtrl.js
+++ assets/js/controllers/userCtrl.js
@@ -5,99 +5,109 @@
 app.controller('UserCtrl', ["$scope", "$state", "flowFactory", "RestService", "toaster", "localStorageService", "$rootScope",
     function ($scope, $state, flowFactory, RestService, toaster, localStorageService, $rootScope) {
 
-    $scope.removeImage = function () {
-        $scope.noImage = true;
-    };
-    $scope.obj = new Flow();
+        $scope.removeImage = function () {
+            $scope.noImage = true;
+        };
+        $scope.obj = new Flow();
 
-    $scope.userInfo = {
-        firstName: 'Peter',
-        lastName: 'Clark',
-        url: 'www.example.com',
-        email: 'peter@example.com',
-        phone: '(641)-734-4763',
-        gender: 'male',
-        zipCode: '12345',
-        city: 'London (UK)',
-        avatar: 'assets/images/avatar-1-xl.jpg',
-        twitter: '',
-        github: '',
-        facebook: '',
-        linkedin: '',
-        google: '',
-        skype: 'peterclark82'
-    };
-    if ($scope.userInfo.avatar == '') {
-        $scope.noImage = true;
-    }
+        $scope.userInfo = {
+            firstName: 'Peter',
+            lastName: 'Clark',
+            url: 'www.example.com',
+            email: 'peter@example.com',
+            phone: '(641)-734-4763',
+            gender: 'male',
+            zipCode: '12345',
+            city: 'London (UK)',
+            avatar: 'assets/images/avatar-1-xl.jpg',
+            twitter: '',
+            github: '',
+            facebook: '',
+            linkedin: '',
+            google: '',
+            skype: 'peterclark82'
+        };
+        if ($scope.userInfo.avatar == '') {
+            $scope.noImage = true;
+        }
 
-    $scope.user = {
-        fullName: '',
-        gender: '',
-        email: '',
-        password: '',
-        id: ''
-    };
+        $scope.user = {
+            fullName: '',
+            gender: '',
+            email: '',
+            password: '',
+            id: ''
+        };
 
-    $scope.password_again = '';
-    $scope.term_check = '';
+        $scope.password_again = '';
+        $scope.term_check = '';
 
-    $scope.updateSessionInfo = function () {
-        localStorageService.set('isLogged', $scope.user.id != '');
-        $rootScope.$broadcast('sessionChanged');
-    }
+        $scope.updateSessionInfo = function () {
+            localStorageService.set('isLogged', $scope.user.id != '');
+            $rootScope.$broadcast('sessionChanged');
+        }
 
-    $scope.signup = function() {
-        if($scope.user.fullName != '' && $scope.user.email != '' &&
-            $scope.user.gender != '' && $scope.user.password != '') {
-            if($scope.user.password == $scope.password_again) {
-                if($scope.term_check == true) {
-                    RestService.createUser($scope.user)
-                        .then(
-                            function(data) {
-                                if(data == -1) {
-                                    toaster.pop('error', 'Error', 'Email in use. Please use another email to sign up.');
+        $scope.signup = function() {
+            if($scope.user.fullName != '' && $scope.user.email != '' &&
+                $scope.user.gender != '' && $scope.user.password != '') {
+                if($scope.user.password == $scope.password_again) {
+                    if($scope.term_check == true) {
+                        RestService.createUser($scope.user)
+                            .then(
+                                function(data) {
+                                    if(data == -1) {
+                                        toaster.pop('error', 'Error', 'Email in use. Please use another email to sign up.');
+                                    }
+                                    else{
+                                        $scope.user.id = data;
+                                        $scope.updateSessionInfo();
+                                        $state.go('app.default');
+                                    }
+                                },
+                                function(errResponse) {
+                                    console.log(errResponse);
                                 }
-                                else{
-                                    $scope.user.id = data;
-                                    $scope.updateSessionInfo();
-                                    $state.go('app.default');
-                                }
-                            },
-                            function(errResponse) {
-                                console.log(errResponse);
-                            }
-                        );
-                } else {
-                    toaster.pop('error', 'Error', 'Please accept the terms and conditions in this step before proceeding.');
+                            );
+                    } else {
+                        toaster.pop('error', 'Error', 'Please accept the terms and conditions in this step before proceeding.');
+                    }
                 }
             }
-        }
-        else {
-            toaster.pop('error', 'Error', 'Please complete the form in this step before proceeding.');
-        }
-    };
+            else {
+                toaster.pop('error', 'Error', 'Please complete the form in this step before proceeding.');
+            }
+        };
 
-    $scope.signin = function () {
-        if($scope.user.email != '' && $scope.user.password != '') {
-            RestService.fetchUser($scope.user.email)
-                .then(
-                    function(data) {
-                        if(data[0].password == $scope.user.password) {
-                            $scope.user.id = data[0].id;
-                            $scope.updateSessionInfo();
-                            $state.go('app.default');
-                        } else {
-                            toaster.pop('error', 'Error', 'Wrong password.');
+        $scope.signin = function () {
+            if($scope.user.email != '' && $scope.user.password != '') {
+                RestService.fetchUser($scope.user.email)
+                    .then(
+                        function(data) {
+                            if(data[0].password == $scope.user.password) {
+                                $scope.user.id = data[0].id;
+                                $scope.updateSessionInfo();
+                                $state.go('app.default');
+                            } else {
+                                toaster.pop('error', 'Error', 'Wrong password.');
+                            }
+                        },
+                        function(errResponse){
+                            console.log(errResponse);
                         }
-                    },
-                    function(errResponse){
-                        console.log(errResponse);
-                    }
-                );
-        } else {
-            toaster.pop('error', 'Error', 'Introduce the username and password.');
-        }
-    };
+                    );
+            } else {
+                toaster.pop('error', 'Error', 'Introduce the username and password.');
+            }
+        };
 
-}]);
+        $scope.logout = function () {
+            if(localStorageService.get('isLogged')) {
+                localStorageService.set('isLogged', false);
+                $rootScope.$broadcast('sessionChanged');
+                $state.go('app.default');
+            }
+            else {
+                toaster.pop('error', 'Error', 'Not logged in.');
+            }
+        }
+    }]);

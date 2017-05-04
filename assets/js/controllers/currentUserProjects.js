2cd52f1d1692056f93fae3f435cb6c4a603d37fd
alejandro ravelo julian <aravelo@uci.cu>
2017-05-04 11:35:53 -0400

    modified project_user.html

--- assets/js/controllers/currentUserProjects.js
+++ assets/js/controllers/currentUserProjects.js
@@ -5,6 +5,7 @@
 app.controller('CurrentUserProjects', ["$scope", "localStorageService", "RestService",
     function ($scope, localStorageService, RestService) {
         $scope.simpleProjects = [];
+        $scope.owner = '';
 
         $scope.getProjects = function () {
             RestService.fetchSimpleProjects(localStorageService.get('currentUserId'))
@@ -19,4 +20,21 @@ app.controller('CurrentUserProjects', ["$scope", "localStorageService", "RestSer
         };
 
         $scope.getProjects();
+
+        $scope.getOwnerData = function () {
+            RestService.fetchUser(localStorageService.get('currentUserId'))
+                .then(
+                    function(data) {
+                        $scope.owner = data[0];
+                    },
+                    function(errResponse){
+                        console.log(errResponse);
+                    }
+                );
+        };
+
+        $scope.getOwnerData();
+
+        $scope.stateArray = ['','In Preparation', 'Active: On time', 'Active: Best than expected','Active: Delayed', 'Finished'];
+
     }]);

37a851bfab65a7543c58924ccdc49dbf8c51c5bb
tony <tony@zimtronic.com>
2017-05-10 22:38:03 -0400

    dividing tornado server on different files

---
+++ server/handlers/proposal.py
@@ -0,0 +1,33 @@
+from datetime import date
+import tornado.escape
+import tornado.ioloop
+import tornado.web
+import tornado.httpserver
+import tornado.httputil
+import tornado.websocket
+import json
+import logging
+
+from databases.coralliumTiny import *
+
+class ProposalByProjectIdHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        print("setting headers!!!")
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding")
+        self.set_header('Access-Control-Allow-Methods', "POST, GET, OPTIONS, DELETE, PUT")
+    
+    def options(self):
+        print('options!!!')
+        self.set_status(204)
+        self.finish()
+
+    def get(self, projectId):
+        print('ProposalByProjectIdHandler:GET!!!')
+
+        print('projectId: ' + projectId)
+
+        proposals = table_proposal.search((where('projectId') == int(projectId)) | (where('projectId') == projectId))
+        self.write(json.dumps(proposals))
+
+        print(proposals)
---
+++ server/handlers/task.py
@@ -0,0 +1,70 @@
+from datetime import date
+import tornado.escape
+import tornado.ioloop
+import tornado.web
+import tornado.httpserver
+import tornado.httputil
+import tornado.websocket
+import json
+import logging
+
+from databases.coralliumTiny import *
+
+
+class TaskHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        print("setting headers!!!")
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding")
+        self.set_header('Access-Control-Allow-Methods', "POST, GET, OPTIONS, DELETE, PUT")
+    
+    def options(self):
+        print('options!!!')
+        self.set_status(204)
+        self.finish()
+
+    def get(self, id):
+        print('Task:GET!!!')
+        print('id: ' + id)
+
+        task = table_task.search(where('id') == int(id))
+        self.write(json.dumps(task))
+        print(task)
+
+    def post(self):
+        print("Task:POST!!!")
+
+        self.json_args = json.loads(self.request.body)
+        print(self.json_args)
+        print(self.json_args['projectId'])
+
+        if len(table_simple_project.search(where('id') == int(self.json_args['projectId']))) != 0:
+            id = table_task.insert(self.json_args)
+            table_task.update({'id': id}, eids=[id])
+            self.write(str(id))
+            print(id)
+        else:
+            self.write('-1')
+            print('ProjectId not found') 
+            
+class TaskByProjectIdHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        print("setting headers!!!")
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding")
+        self.set_header('Access-Control-Allow-Methods', "POST, GET, OPTIONS, DELETE, PUT")
+    
+    def options(self):
+        print('options!!!')
+        self.set_status(204)
+        self.finish()
+
+    def get(self, projectId):
+        print('TaskByProjectIdHandler:GET!!!')
+
+        print('projectId: ' + projectId)
+
+        tasks = table_task.search(where('projectId') == projectId)
+        self.write(json.dumps(tasks))
+
+        print(tasks) 

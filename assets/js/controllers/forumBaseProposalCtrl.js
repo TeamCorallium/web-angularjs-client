799419d2f90b0e4ecc2702ae3b49601ede7cc1d9
alejandro ravelo julian <aravelo@uci.cu>
2017-06-02 16:07:26 -0400

    modified forum and proposal

--- assets/js/controllers/forumBaseProposalCtrl.js
+++ assets/js/controllers/forumBaseProposalCtrl.js
@@ -24,6 +24,7 @@ app.controller('ForumBaseProposalCtrl', ["$scope", "$state", "toaster", "WebSock
         $scope.proposalContent = '';
         $scope.itemSubject = '';
         $scope.currentForumActive = '';
+        $scope.proposalType = '';
 
         //Actual proposal of the current project
         $scope.currentProposal = {
@@ -34,6 +35,8 @@ app.controller('ForumBaseProposalCtrl', ["$scope", "$state", "toaster", "WebSock
             projectId: '',
             proposalOwnerId: '',
             state: '',
+            type: '',
+            deathLine: ''
         };
 
         $scope.createProposal = function () {
@@ -43,14 +46,92 @@ app.controller('ForumBaseProposalCtrl', ["$scope", "$state", "toaster", "WebSock
             $scope.currentProposal.projectId = localStorageService.get('currentProjectId');
             $scope.currentProposal.proposalOwnerId = localStorageService.get('currentUserId');
             $scope.currentProposal.state = 'publish';
+            $scope.currentProposal.type = $scope.proposalType;
+            $scope.currentProposal.deathLine = $scope.deathLine;
 
             var obj = {
                 type: 'PROPOSAL',
                 value: $scope.currentProposal
             };
             WebSocketService.send(obj);
-            // WebSocketService.send($scope.currentProposal);
 
             $state.go('app.forum.base');
         };
+
+        //Date picker
+        $scope.today = function() {
+            $scope.deathLine = new Date();
+        };
+        $scope.today();
+        // $scope.start = $scope.minDate;
+        $scope.start = $scope.deathLine;
+        $scope.end = $scope.maxDate;
+        $scope.clear = function() {
+            $scope.dt = null;
+        };
+        $scope.datepickerOptions = {
+            showWeeks : false,
+            startingDay : 1
+        };
+        $scope.dateDisabledOptions = {
+            dateDisabled : disabled,
+            showWeeks : false,
+            startingDay : 1
+        };
+        $scope.startOptions = {
+            showWeeks : false,
+            startingDay : 1,
+            minDate: $scope.minDate,
+            maxDate: $scope.maxDate
+        };
+        $scope.endOptions = {
+            showWeeks : false,
+            startingDay : 1,
+            minDate: $scope.minDate,
+            maxDate: $scope.maxDate
+        };
+        // Disable weekend selection
+        function disabled(data) {
+            var date = data.date, mode = data.mode;
+            return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+        }
+        $scope.setDate = function(year, month, day) {
+            $scope.deathLine = new Date(year, month, day);
+        };
+        $scope.toggleMin = function() {
+            $scope.datepickerOptions.minDate = $scope.datepickerOptions.minDate ? null : new Date();
+            $scope.dateDisabledOptions.minDate = $scope.dateDisabledOptions.minDate ? null : new Date();
+        };
+        $scope.maxDate = new Date(2020, 5, 22);
+        $scope.minDate = new Date(1970, 12, 31);
+        $scope.open = function() {
+            $scope.opened = !$scope.opened;
+        };
+        $scope.endOpen = function() {
+            $scope.endOptions.minDate = $scope.start;
+            $scope.startOpened = false;
+            $scope.endOpened = !$scope.endOpened;
+        };
+        $scope.startOpen = function() {
+            $scope.startOptions.maxDate = $scope.end;
+            $scope.endOpened = false;
+            $scope.startOpened = !$scope.startOpened;
+        };
+        $scope.dateOptions = {
+            formatYear : 'yy',
+            startingDay : 1
+        };
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+
+        $scope.hstep = 1;
+        $scope.mstep = 15;
+
+        $scope.monthArray = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+        $scope.getProjectDate = function (date) {
+            var dateTemp = new Date(date);
+            return $scope.monthArray[dateTemp.getMonth()] + " " + dateTemp.getDate() + ", "+ dateTemp.getFullYear();
+        };
+
     }]);

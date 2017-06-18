6ffdae5de5e769a3039d8a89c91e4d59b753f45c
tony <tony@zimtronic.com>
2017-06-17 23:27:29 -0400

    change accordions on default home page

--- assets/js/controllers/vAccordionCtrl.js
+++ assets/js/controllers/vAccordionCtrl.js
@@ -15,18 +15,16 @@
         };
         $scope.panes = [{
             header: 'System objetives',
-            content:   `<div class="list-group">
-                            <a class="list-group-item list-group-item-warning" href="#">To create a communication space for bringing together potential Financers and Projects Owners or entrepreneurs looking for capital.</a>
-                            <a class="list-group-item list-group-item-warning" href="#">To facilitate the preparation of Projects to entrepreneurs.</a>
-                            <a class="list-group-item list-group-item-warning" href="#">To create an environment for the creation and management of Project Teams.</a>
-                            <a class="list-group-item list-group-item-warning" href="#">To manage the Capital invested by Financers to the execution of projects.</a>
-                        </div>`
+            content:   `    <p>To create a communication space for bringing together potential Financers and Projects Owners or entrepreneurs looking for capital.</p>
+                            <p>To facilitate the preparation of Projects to entrepreneurs.</p>
+                            <p>To create an environment for the creation and management of Project Teams.</p>
+                            <p>To manage the Capital invested by Financers to the execution of projects.</p>
+                        `
         }, {
             header: 'System actors',
-            content: `<div class="list-group">
-                            <a class="list-group-item list-group-item-warning" href="#">Owner: owner of the main assets, property or product who will present a Project, owner of a business idea, person, organization or community who has assets they can increase its production or results with some Capital Investment or financial help.</a>
-                            <a class="list-group-item list-group-item-warning" href="#">Financier: person or organization capable of giving a Capital Investment or financial help to certain owners.</a>
-                        </div>`
+            content: `<p>Owner: owner of the main assets, property or product who will present a Project, owner of a business idea, person, organization or community who has assets they can increase its production or results with some Capital Investment or financial help.</p>
+                      <p>Financier: person or organization capable of giving a Capital Investment or financial help to certain owners.</p>
+                     `
         }, 
         // {
         //     header: 'Pane 3',

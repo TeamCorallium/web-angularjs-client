3f484e9baf974fd470c231e13c35d6a0a551abeb
tony <tony@zimtronic.com>
2017-06-23 15:10:48 -0400

    modified server

--- server/handlers/invertion.py
+++ server/handlers/invertion.py
@@ -61,7 +61,12 @@ class InvertionHandler(tornado.web.RequestHandler):
         table_simple_project.update({'inverted': True}, eids=[int(projectId)])
 
         table_activity.insert({'userId': userId, 'title': 'Invertion', 
-                               'content': "You make an invertion", 'date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+                               'content': "You made an invertion", 'date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+
+        
+        # table_notification.insert({'userId': userId, 'projectId': projectId, 'proposalId': '', 'from': fromName, 'read': False, 'type': notificationType, 'date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                                    # 'subject': "Invertion", 'content': "ou made an invertion"})
+
 
 class TransactionByProjectIdHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
